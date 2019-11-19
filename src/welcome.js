39b745fff4dc9f52df85802100e15c1f1987c3c7
Daniele <d.suppo@issimissimo.com>
2019-11-19 23:50:44 +0100

    map loader

--- src/welcome.js
+++ src/welcome.js
@@ -1,11 +1,45 @@
-$(document).ready(function () {
-    $("#welcomePanel").css('display', 'block');
-});
 
-function hideWelcomePanel(){
-    $(document).ready(function () {
+const mapLoader = {
+    minProgress: 10, /// change only this for min requested tiles
+    maxProgress: 0,
+    isProgressing: false,
+
+    onProgress: (valProgress) => {
+        if (!mapIsReady) {
+            if (!mapLoader.isProgressing) {
+                /// flag as started to download tiles
+                if (valProgress >= mapLoader.minProgress) {
+                    mapLoader.isProgressing = true;
+                    mapLoader.maxProgress = valProgress;
+                }
+            }
+            /// get the progress in percentage
+            if (mapLoader.isProgressing) {
+                let mapLoadingPercent = (100 - (valProgress / mapLoader.maxProgress * 100)).toFixed(0);
+                // console.log('map loading : ' + mapLoadingPercent + '%');
+                $("#mapLoaderIndicator").text(mapLoadingPercent + '%');
+            }
+        }
+        else{
+            mapLoader.hideLoader();
+        }
+    },
+
+    showLoader: () => {
+        $("#welcomePanel").css('display', 'block');
+    },
+
+    hideLoader: () => {
         $("#welcomePanel").fadeOut();
-    });
+    },
 }
 
-mapController.onMapReayHandlers.push(hideWelcomePanel);
+
+/// register the handler to catch the tile loading progress
+viewer.scene.globe.tileLoadProgressEvent.addEventListener((valProgress) => {
+    mapLoader.onProgress(valProgress);
+});
+
+
+/// initialize with the map loader visible
+$(document).ready(mapLoader.showLoader());
